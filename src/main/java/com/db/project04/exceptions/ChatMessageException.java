df48938d9c14c9d68176510cc668acf4f9fb176c
SpringSpark <artaszx@mail.ru>
2018-08-31 14:52:52 +0400

    Add test structure.

---
+++ src/main/java/com/db/project04/exceptions/ChatMessageException.java
@@ -0,0 +1,7 @@
+package com.db.project04.exceptions;
+
+public class ChatMessageException extends ChatException {
+    public ChatMessageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/test/java/com/db/project04/MessageTest.java
@@ -0,0 +1,4 @@
+package com.db.project04;
+
+public class MessageTest {
+}
