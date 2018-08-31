fd2bb21c86ae5eb3482476f5262469bd9a6d5943
Timur Baev <timurbaev1996@yandex.ru>
2018-08-31 16:20:21 +0300

    add throw to test

--- src/test/java/com/db/project04/CommandControllerTest.java
+++ src/test/java/com/db/project04/CommandControllerTest.java
@@ -5,6 +5,7 @@ import com.db.project04.command.CommandController;
 import com.db.project04.command.HistoryCommand;
 import com.db.project04.command.SendMessageCommand;
 import com.db.project04.exceptions.ChatMessageException;
+import com.db.project04.exceptions.ChatParseCommandException;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -16,27 +17,27 @@ public class CommandControllerTest {
 
     @Ignore
     @Test
-    public void shouldCreateHistoryCommand(){
+    public void shouldCreateHistoryCommand() throws ChatParseCommandException {
         ChatCommand resultCommand = sut.parseCommand("/hist");
         assertTrue(resultCommand instanceof HistoryCommand);
     }
 
     @Ignore
     @Test
-    public void shouldCreateSendMessageCommand(){
+    public void shouldCreateSendMessageCommand() throws ChatParseCommandException {
         ChatCommand resultCommand = sut.parseCommand("/snd test message");
         assertTrue(resultCommand instanceof SendMessageCommand);
     }
 
     @Ignore
     @Test(expected = ChatMessageException.class)
-    public void shouldThrowChatMessageExceptionWhenMessageIsEmpty(){
+    public void shouldThrowChatMessageExceptionWhenMessageIsEmpty() throws ChatParseCommandException {
         ChatCommand resultCommand = sut.parseCommand("/snd ");
     }
 
     @Ignore
     @Test(expected = ChatMessageException.class)
-    public void shouldThrowChatMessageExceptionWhenUndefinedCommand(){
+    public void shouldThrowChatMessageExceptionWhenUndefinedCommand() throws ChatParseCommandException {
         ChatCommand resultCommand = sut.parseCommand("/wrong ");
     }
 }
