8ad921c9480f09c5d60563cf87e28d0d7a7beaa7
Timur Baev <timurbaev1996@yandex.ru>
2018-08-31 16:35:55 +0300

    add sendmessagecommandtest

--- src/main/java/com/db/project04/command/SendMessageCommand.java
+++ src/main/java/com/db/project04/command/SendMessageCommand.java
@@ -5,6 +5,8 @@ import com.db.project04.exceptions.ChatMessageException;
 import static java.util.Objects.isNull;
 
 public class SendMessageCommand extends ChatCommand {
+
+    public static final int MAX_LENGTH = 150;
     private String handledString;
 
     public SendMessageCommand(String messageString) throws ChatMessageException {
@@ -12,7 +14,7 @@ public class SendMessageCommand extends ChatCommand {
             throw new ChatMessageException("empty input message");
         }
         messageString = messageString.trim();
-        if(messageString.length() > 150) {
+        if(messageString.length() > MAX_LENGTH) {
             throw new ChatMessageException("input message is longer than 150 characters");
         }
         handledString = messageString;
