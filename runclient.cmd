938358b63576ffbfd3bbf2b2c1178659f01a48a8
SpringSpark <artaszx@mail.ru>
2018-09-06 17:19:58 +0400

    Modify *.cmd files.

--- runclient.cmd
+++ runclient.cmd
@@ -1,1 +1,1 @@
-java -cp C:\Users\Java_9\Documents\Java\project04\target\project04-1.0-SNAPSHOT.jar com.db.project04.client.ClientMain
+java -cp %~dp0target\project04-1.0-SNAPSHOT.jar com.db.project04.client.ClientMain
