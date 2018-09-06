aecf0f847a49a4c792c0d53383cabfd903a08c2a
SpringSpark <artaszx@mail.ru>
2018-09-06 16:58:09 +0400

    Implement login in history.

--- src/main/java/com/db/project04/command/ServerCommandController.java
+++ src/main/java/com/db/project04/command/ServerCommandController.java
@@ -10,57 +10,38 @@ import static java.util.Objects.isNull;
 
 public class ServerCommandController extends CommandController{
 
-    public static final Pattern SND_COMMAND_STRING_PATTERN = Pattern.compile("(\\S+) (.+)");
+    public static final Pattern PREPARATION_COMMAND_STRING_PATTERN = Pattern.compile("(\\S+) (.+)");
     public static final Pattern COMMAND_STRING_PATTERN = Pattern.compile("\\/([A-Za-z]+)(.*)");
 
     //(.+) (.+)
     public static ChatCommand parseCommand(String rawString) throws ChatException {
 
-        System.out.println("lets's parse string " + rawString);
-        Matcher sndCommandStringPatternMatcher = SND_COMMAND_STRING_PATTERN.matcher(rawString);
-        if(sndCommandStringPatternMatcher.matches())
-        {
-            String rawStringWithoutUsername = sndCommandStringPatternMatcher.group(2);
-            System.out.println("it's send command; let's parse " + rawStringWithoutUsername);
-            return matchString(rawStringWithoutUsername, true);
-        }
-        else {
-            return matchString(rawString, false);
-        }
-    }
+        Matcher sndCommandStringPatternMatcher = PREPARATION_COMMAND_STRING_PATTERN.matcher(rawString);
+        if (sndCommandStringPatternMatcher.matches()) {
+            String username = sndCommandStringPatternMatcher.group(1);
+            String command = sndCommandStringPatternMatcher.group(2);
 
-    public static ChatCommand matchString(String rawString, boolean snd) throws ChatException {
-        Matcher commandStringPatternMatcher = COMMAND_STRING_PATTERN.matcher(rawString);
-        if (commandStringPatternMatcher.matches()) {
-            if(snd == true)
-            {
-                return createSndChatCommand(commandStringPatternMatcher);
-            }
-            else {
-                return createConcreteChatCommand(commandStringPatternMatcher);
+
+            Matcher commandStringPatternMatcher = COMMAND_STRING_PATTERN.matcher(command);
+            if (commandStringPatternMatcher.matches()) {
+                return createConcreteChatCommand(commandStringPatternMatcher, username);
             }
         }
         throw new ChatParseCommandFormatException("command has incorrect format");
     }
 
-    private static ChatCommand createConcreteChatCommand(Matcher commandStringPatternMatcher)
+    private static ChatCommand createConcreteChatCommand(Matcher commandStringPatternMatcher, String username)
             throws ChatException {
         String commandStringName = commandStringPatternMatcher.group(1);
+        if (Objects.equals(commandStringName, RobustSendMessageCommand.COMMAND_NAME)) {
+            String messageText = commandStringPatternMatcher.group(2);
+            return (new SendMessageCommand(username + messageText));
+        }
         if (Objects.equals(commandStringName, HistoryCommand.COMMAND_NAME)) {
             return new HistoryCommand();
         }
         if (Objects.equals(commandStringName, ClientShutdownCommand.COMMAND_NAME)) {
             return  new ClientShutdownCommand();
-        }
-        throw new ChatParseCommandTypeException("unable to parse command type");
-    }
-
-    private static ChatCommand createSndChatCommand(Matcher commandStringPatternMatcher)
-            throws ChatException {
-        String commandStringName = commandStringPatternMatcher.group(1);
-        if (Objects.equals(commandStringName, RobustSendMessageCommand.COMMAND_NAME)) {
-            String messageText = commandStringPatternMatcher.group(2);
-            return (new SendMessageCommand(messageText));
         }
         throw new ChatParseCommandTypeException("unable to parse command type");
     }
