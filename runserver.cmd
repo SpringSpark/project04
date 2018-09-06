505377225b6cb84d39bc89c9a722c14fcf23ec33
Timur Baev <timurbaev1996@yandex.ru>
2018-09-06 17:10:01 +0300

    fix run script

--- runserver.cmd
+++ runserver.cmd
@@ -1,2 +1,2 @@
-java -cp C:\Users\Java_9\Documents\Java\project04\target\project04-1.0-SNAPSHOT.jar com.db.project04.server.Server
+java -cp C:\Users\Java_9\Documents\Java\project04\target\project04-1.0-SNAPSHOT.jar com.db.project04.server.ServerMain
 
