920bcc91bdd3e0307a8bb9bd02ba8675d7ae281e
Timur Baev <timurbaev1996@yandex.ru>
2018-09-07 13:56:46 +0300

    Add tests fro Client.

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,10 +1,27 @@
 import com.db.project04.client.Client;
 import com.db.project04.exceptions.ChatClientException;
+import com.db.project04.server.Server;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class ClientTest {
     @Test (expected = ChatClientException.class)
     public void clientShouldExitWhenThereIsNoServer() throws ChatClientException {
         Client client = new Client();
+    }
+
+    @Test
+    public void clientSetUserName() throws IOException, ChatClientException {
+        Server server = new Server();
+        server.start();
+        Client client = new Client();
+        client.setUsername("user_name");
+        assertEquals("user_name", client.getUsername());
+        server.stop();
     }
 }
